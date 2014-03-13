30ae7ac9b0a825ddf7026071c333c41a11fecf6e
Maks Verver <maksverver@geocities.com>
2014-03-13 18:16:09 +0100

    Simplify SimplePlayer3 by just inhereting from SimplePlayer2.

--- SimplePlayer3.h
+++ SimplePlayer3.h
@@ -1,24 +1,15 @@
 #ifndef SIMPLE_PLAYER3_H_INCLUDED
 #define SIMPLE_PLAYER3_H_INCLUDED
 
-#include "Player.h"
+#include "SimplePlayer2.h"
 
-class SimplePlayer3 : public Player
+class SimplePlayer3 : public SimplePlayer2
 {
     virtual std::vector<int> pick_starting_countries(
         const World      &world,
         std::vector<int> options,
         int              num_countries,
         int              timeout_ms );
-
-    virtual std::vector<Placement> place_armies(
-        const World &world,
-        int         num_armies,
-        int         timeout_ms );
-
-    virtual std::vector<Movement> attack_transfer(
-        const World &world,
-        int         timeout_ms );
 
 protected:
     int best_continent;
